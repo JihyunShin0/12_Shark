ca77cf4d018edc726aa575ea8b1a918c25da0257
JihyunShin0 <jirung@sookmyung.ac.kr>
2025-11-18 15:25:14 +0900

    12_01

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    srand( (unsigned) time(NULL) );
+    
+    printf("======================================================\n");
+    printf("                                                      \n");
+    printf("               < SHARK ISLAND GAME >                  \n");
+    printf("                                                      \n");
+    printf("======================================================\n\n\n");
+    
+    
+    
+    printf("\n\n\n\n\n");
+    printf("======================================================\n");
+    printf("                   ^^^^^^^^^^^^^^                     \n");
+    printf("                 < CONGRATULATION >                   \n");
+    printf("                   vvvvvvvvvvvvvv                     \n");
+    printf("                 YOU WIN!!!!!!!!!!!                   \n");
+    printf("======================================================\n\n\n");
+    
+    
+    
+    system("PAUSE");	
+    return 0;
+}
